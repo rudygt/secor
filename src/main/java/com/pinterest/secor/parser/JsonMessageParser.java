7730290e68904e183e22bf122b02121f07fca49e
Rudy Alvarez <ralvarez@katch.com>
2017-04-06 11:27:01 -0600

    fixed missing class

--- src/main/java/com/pinterest/secor/parser/JsonMessageParser.java
+++ src/main/java/com/pinterest/secor/parser/JsonMessageParser.java
@@ -52,27 +52,3 @@ public class JsonMessageParser extends TimestampedMessageParser {
     }
 
 }
-
-public class JsonDateMessageParser extends TimestampedMessageParser {
-    private final boolean m_timestampRequired;
-
-    public JsonDateMessageParser(SecorConfig config) {
-        super(config);
-        m_timestampRequired = config.isMessageTimestampRequired();
-    }
-
-    @Override
-    public long extractTimestampMillis(final Message message) {
-        JSONObject jsonObject = (JSONObject) JSONValue.parse(message.getPayload());
-        if (jsonObject != null) {
-            Object fieldValue = getJsonFieldValue(jsonObject);
-            if (fieldValue != null) {
-                return toMillis(DatatypeConverter.parseDateTime(fieldValue.toString()).getTime().getTime());
-            }
-        } else if (m_timestampRequired) {
-            throw new RuntimeException("Missing timestamp field for message: " + message);
-        }
-        return 0;
-    }
-
-}
